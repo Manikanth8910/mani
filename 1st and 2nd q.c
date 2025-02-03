bdbb50145b21bd95e43d491f4307911c1c68bfe4
Manikanth8910 <24071a05b9@vnrvjiet.in>
2025-02-04 00:33:11 +0530

    Add files via upload

---
+++ 1st and 2nd q.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+//int main(){
+  //  int a;
+   // printf("enter number :");
+   // scanf("%d",&a);
+   //  (a%2==0)?printf("the given number is even\n"):printf("the given number is odd\n");
+   // printf("thankyou");
+   // return 0;
+//
+//int main(){
+   // int x;
+   // printf("enter x :");
+   // scanf("%d", &x);
+   // if ((x&1)==1){
+//printf("the given number is odd");
+   // }else{
+   //     printf("the given number is even");
+  //  }
+   // return 0;
+//}
+// more ways to check even or odd are x|1=x odd,,x|1=x+1  even
+//x^1=x+1 even,,,x^1=x-1  odd
